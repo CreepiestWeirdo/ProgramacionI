23fb9a36d4f15fae81a764e89722ff218dd269e7
Federico Poliseno <federicopoliseno@yahoo.com.ar>
2024-08-22 20:47:05 -0300

    update

--- Taller de Programación/tablamultiplicar.py
+++ Taller de Programación/tablamultiplicar.py
@@ -4,7 +4,7 @@
 numero = int(input("Introduce un numero para ver su tabla de multiplicar: "))
 
 #Generar tabla de multiplicar
-print(f"Tabla de multiplicar del {numero}: \n")
+#print(f"Tabla de multiplicar del {numero}:")
 for num in range(1, 11):
     print(f"{numero} x {num} = {numero * num}")
 
