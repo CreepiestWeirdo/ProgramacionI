4a2a2a8cdfe6ed48e31fa81f21f59ff4875ae43e
Federico Poliseno <federicopoliseno@yahoo.com.ar>
2024-08-22 22:07:21 -0300

    update tarea

---
+++ Clase 6 - 22 de agosto/listas.py
@@ -0,0 +1,30 @@
+# Lista> coleccion ordenada de elementos que pueden ser de cualquier tipo de datos.
+
+mi_lista = [1, 2, 3, 4, 5, 2, 4, 1]
+
+# Caracteristicas> 
+# son mutables, los elementos pueden modificados
+# pueden contener elementos de distintos tipos de datos
+# soportan duplicados 
+
+frutas = ["manzana", "banana", "cereza", "pera", "naranja"]
+
+print(frutas)
+
+print(frutas[1]) # acceso de datos por indexacion, entre corchetes se cita el indice
+
+print(type(frutas))
+print(type(frutas[2]))
+
+print(frutas[2])
+print(frutas[-1])
+print(frutas[1 : 4]) #slicing
+print(frutas[1 : -1]) 
+
+
+#TAREA PARA EL LUNES, RECORRER LA LISTA CON FOR 
+for fruta in frutas:
+
+    while i < 4:
+        print(frutas[i])
+        i += 1
